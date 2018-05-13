3444956869a8e27786bdb362bc68d390754d7b6b
jwang1013 <37950852+jwang1013@users.noreply.github.com>
2018-05-13 02:39:35 -0700

    change month get method

--- server/pollDb.js
+++ server/pollDb.js
@@ -94,7 +94,7 @@ function getMyDate() {
     var date = new Date();
     var day = date.getDate();
     if (day < 10) day = '0' + day;
-    var month = date.getMonth();
+    var month = date.getMonth() + 1;
     if (month < 10) month = '0' + month;
     var year = date.getFullYear();
     return year + '-' + month + '-' + day;
